94b2e740a689885da6209f019ee7ca564868d47c
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-27 01:37:35 +0100

    media query added for header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css';
+import logo from './meal-icon.png';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='header'>
+                <div className='banner'>
+                    <img src={logo} alt="" />
+                    <p>The Meal DB</p>
+                </div>
+                <div>
+                    <a href="./home">Home</a>
+                    <a href="./shop">Shop</a>
+                    <a href="./about-us">About Us</a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
